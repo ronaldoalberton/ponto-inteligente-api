f7b71ea01a15273a13d3a5588a3b779bbcbeb3c9
Ronaldo Alberton <ronaldo.alberton@ciss.com.br>
2018-01-14 19:41:29 -0200

    add supporte a travis

--- src/main/java/com/pontointeligente/api/PontoInteligenteApiApplication.java
+++ src/main/java/com/pontointeligente/api/PontoInteligenteApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PontoInteligenteApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PontoInteligenteApiApplication.class, args);
 	}
