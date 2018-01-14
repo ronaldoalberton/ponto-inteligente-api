213dddde1f44e5ef575371edd77934c621395aa9
Ronaldo Alberton <ronaldo.alberton@ciss.com.br>
2018-01-14 19:33:16 -0200

    Commit inicial

---
+++ src/main/java/com/pontointeligente/api/PontoInteligenteApiApplication.java
@@ -0,0 +1,12 @@
+package com.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoInteligenteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligenteApiApplication.class, args);
+	}
+}
